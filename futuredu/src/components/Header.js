2ba3c78c92adb609a1c6ba597bdb0142a25aec4f
Neeraj Chouhan <48256974+NeerajChouhan7@users.noreply.github.com>
2020-10-11 00:32:13 +0530

    Update Header.js

--- futuredu/src/components/Header.js
+++ futuredu/src/components/Header.js
@@ -2,8 +2,27 @@ import React from "react"
 
 function Header(){
     return (
-        <div>This will be the Header</div>
+        <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#">FuturEdu</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home</a>
+                </li>
+                <li className="nav-item  active">
+                    <a className="nav-link" href="#">Future Tech</a>
+                </li>
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Skills</a>
+                </li>
+            </ul>
+            </div>
+        </nav>
+        </div>
     )
 }
-
 export default Header;
