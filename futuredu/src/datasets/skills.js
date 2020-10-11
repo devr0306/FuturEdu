da2a5f83f98300a9d08d3db3a54791f29e4bc8d5
devr0306 <devr0306@gmail.com>
2020-10-11 11:53:48 -0500

    Nothing

---
+++ futuredu/src/datasets/skills.js
@@ -0,0 +1,142 @@
+module.exports = [
+    {
+        name: "Machine Learning",
+        description: "",
+        subtopicID: 0,
+    },
+    {
+        name: "Deep Learning",
+        description: "",
+        subtopicID: 0,
+    },
+    {
+        name: "Atoms and Molecular Structures",
+        description: "",
+        subtopicID: 1,
+    },
+    {
+        name: "Reactions and Kinetics",
+        description: "",
+        subtopicID: 1,
+    },
+    {
+        name: "States of Matter and IMFs ",
+        description: "",
+        subtopicID: 1,
+    },
+    {
+        name: "Equilibrium, Acids and Bases",
+        description: "",
+        subtopicID: 1,
+    },
+    {
+        name: "Thermodynamics and Electrochemistry",
+        description: "",
+        subtopicID: 1,
+    },
+    {
+        name: "Mechanics",
+        description: "Study the motion of objects, and forces causing the motion. Also includes energy and momentum.",
+        subtopicID: 2,
+    },
+    {
+        name: "Electricity and Magnetism",
+        description: "The forces and properties of electrical and magnetic fields.",
+        subtopicID: 2,
+    },
+    {
+        name: "Waves",
+        description: "",
+        subtopicID: 2,
+    },
+    {
+        name: "Fluids and Thermodynamics",
+        description: "",
+        subtopicID: 2,
+    },
+    {
+        name: "Quantum Physics",
+        description: "",
+        subtopicID: 2,
+    },
+    {
+        name: "Chemical Biology",
+        description: "",
+        subtopicID: 3,
+    },
+    {
+        name: "Cells",
+        description: "",
+        subtopicID: 3,
+    },
+    {
+        name: "Genetics",
+        description: "",
+        subtopicID: 3,
+    },
+    {
+        name: "Natural Selection and Ecology",
+        description: "",
+        subtopicID: 3,
+    },
+    {
+        name: "Java",
+        description: "One of the most popular programming languages which is Object Oriented. Great for learning concepts but harder to learn than languages like python. One of the languages used in Android App development.",
+        subtopicID: 4,
+    },
+    {
+        name: "Python",
+        description: "Versatile language which has many applications, and is easy to learn as well. Great place to get introduced to Computer Science.",
+        subtopicID: 4,
+    },
+    {
+        name: "HTML & CSS",
+        description: "The fundamental languages of web development which are used to create the design on websites.",
+        subtopicID: 4,
+    },
+    {
+        name: "JavaScript",
+        description: "",
+        subtopicID: 4,
+    },
+    {
+        name: "Algebra",
+        description: "The field involving solving equations and graphing functions.",
+        subtopicID: 5,
+    },
+    {
+        name: "Geometry and Trigonometry",
+        description: "The field of Math involving shapes, with Trigonometry involving Triangles.",
+        subtopicID: 5,
+    },
+    {
+        name: "Calculus",
+        description: "The field of Math involving finding values such as slopes of or areas under curves.",
+        subtopicID: 5,
+    },
+    {
+        name: "Linear Algebra",
+        description: "The field of Math involving Vectors and Matrices.",
+        subtopicID: 5,
+    },
+    {
+        name: "Statistics",
+        description: "The field of Math involving the usage of and calculations on data to understand the world.",
+        subtopicID: 5,
+    },
+    {
+        name: "Beginner Series",
+        description: "",
+        subtopicID: 6,
+    },
+    {
+        name: "Intermediate Series",
+        description: "",
+        subtopicID: 6,
+    },
+    {
+        name: "Advanced Series",
+        description: "",
+        subtopicID: 6,
+    },
+]
