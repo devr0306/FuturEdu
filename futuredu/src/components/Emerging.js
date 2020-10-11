7b673494d3bf6b76932bc416957d16e7251213de
devr0306 <devr0306@gmail.com>
2020-10-11 11:03:04 -0500

    Added some text

--- futuredu/src/components/Emerging.js
+++ futuredu/src/components/Emerging.js
@@ -1,20 +1,43 @@
 import React from "react";
 
-import Dropdown from "./Dropdown";
+import Card from "./Card";
 
-function Emerging(){
-    return(
-        <div>
-            <Dropdown
-                topic="Machine Learning"
-                prerequisites="some prerequisites"
-                description="Some description"
-            />
-            <Dropdown
-                topic="Deep Learning"
-                prerequisites="some prerequisites"
-                description="Some description"
-            />
+import substopics from "../datasets/subtopics";
+
+function Emerging() {
+    return (
+        <div className="">
+            <div className="row">
+                {substopics.filter(s => s.topicID == 1).map(s => {
+                    return (
+                        <div className="col-md-4 mb-3">
+                            <Card
+                                topic={s.name}
+                                id="phy"
+                                image={s.imageURL}
+                                description={s.description}
+                                prerequisites="Machine Learning
+                                -	Models
+                                o	Machine Learning by Stanford- https://www.coursera.org/learn/machine-learning
+                                o	YouTube- https://www.youtube.com/playlist?list=PLoROMvodv4rMiGQp3WXShtMGgzqpfVfbU
+                                
+                                -	Using Models
+                                o	Scikit-Learn- https://www.youtube.com/watch?v=pqNCD_5r0IU
+                                
+                                Deep Learning
+                                -	Models
+                                o	Deep Learning- https://www.youtube.com/watch?v=VyWAvY2CF9c
+                                o	MIT Course- https://www.youtube.com/watch?v=FkHWKq86tSw&list=PLtBw6njQRU-rwp5__7C0oIVt26ZgjG9NI&index=28
+                                
+                                -	Using Models
+                                o	TensorFlow- https://www.youtube.com/watch?v=tPYj3fFJGjk
+                                
+                                "
+                            />
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
---
+++ futuredu/src/datasets/links.js
@@ -0,0 +1,502 @@
+module.exports = [
+    {
+        name: "Machine Learning by Stanford",
+        link: "https://www.coursera.org/learn/machine-learning",
+        subskillID: 0
+    },
+    {
+        name: "YouTube",
+        link: "https://www.youtube.com/playlist?list=PLoROMvodv4rMiGQp3WXShtMGgzqpfVfbU",
+        subskillID: 0
+    },
+    {
+        name: "Deep Learning",
+        link: "https://www.youtube.com/watch?v=VyWAvY2CF9c",
+        subskillID: 2
+    },
+    {
+        name: "MIT Course",
+        link: "https://www.youtube.com/watch?v=FkHWKq86tSw&list=PLtBw6njQRU-rwp5__7C0oIVt26ZgjG9NI&index=28",
+        subskillID: 2
+    },
+    {
+        name: "TensorFlow",
+        link: "https://www.youtube.com/watch?v=tPYj3fFJGjk",
+        subskillID: 3
+    },
+    {
+        name: "Scikit-learn",
+        link: "https://www.youtube.com/watch?v=pqNCD_5r0IU",
+        subskillID: 1
+    },
+    {
+        name: "Carnegie Mellon",
+        link: "https://www.youtube.com/playlist?list=PLm3J0oaFux3YL5qLskC6xQ24JpMwOAeJz",
+        subskillID: 4
+    },
+    {
+        name: "UC Berkeley",
+        link: "https://www.youtube.com/playlist?list=PL74Rel4IAsETUwZS_Se_P-fSEyEVQwni7",
+        subskillID: 4
+    },
+    {
+        name: "Atoms",
+        link: " https://www.khanacademy.org/science/ap-chemistry/atoms-compounds-ions-ap",
+        subskillID: 5
+    },
+    {
+        name: "Electronic Structure",
+        link: " https://www.khanacademy.org/science/ap-chemistry/electronic-structure-of-atoms-ap",
+        subskillID: 5
+    },
+    {
+        name: "Periodic Table",
+        link: " https://www.khanacademy.org/science/ap-chemistry/periodic-table-ap",
+        subskillID: 5
+    },
+    {
+        name: "Chemical Bonds",
+        link: " https://www.khanacademy.org/science/ap-chemistry/chemical-bonds-ap",
+        subskillID: 5
+    },
+    {
+        name: "Stoichiometry and Molecular Composition",
+        link: "https://www.khanacademy.org/science/ap-chemistry/stoichiometry-and-molecular-composition-ap",
+        subskillID: 6
+    },
+    {
+        name: "Chemical Reactions",
+        link: " https://www.khanacademy.org/science/ap-chemistry/chemical-reactions-ap",
+        subskillID: 6
+    },
+    {
+        name: "Kinetics",
+        link: " https://www.khanacademy.org/science/ap-chemistry/kinetics-ap",
+        subskillID: 6
+    },
+    {
+        name: "Intermolecular Forces",
+        link: " https://www.khanacademy.org/science/ap-chemistry/states-of-matter-and-intermolecular-forces-ap",
+        subskillID: 7
+    },
+    {
+        name: "Gases",
+        link: " https://www.khanacademy.org/science/ap-chemistry/gases-and-kinetic-molecular-theory-ap",
+        subskillID: 7
+    },
+    {
+        name: "Equilibrium",
+        link: " https://www.khanacademy.org/science/ap-chemistry/chemical-equilibrium-ap",
+        subskillID: 8
+    },
+    {
+        name: "Acids and Bases",
+        link: " https://www.khanacademy.org/science/ap-chemistry/acids-and-bases-ap",
+        subskillID: 8
+    },
+    {
+        name: "Buffers and Solubility Equilibrium",
+        link: " https://www.khanacademy.org/science/ap-chemistry/buffers-titrations-solubility-equilibria-ap",
+        subskillID: 8
+    },
+    {
+        name: "Thermodynamics",
+        link: " https://www.khanacademy.org/science/ap-chemistry/thermodynamics-ap",
+        subskillID: 9
+    },
+    {
+        name: "Electrochemistry",
+        link: " https://www.khanacademy.org/science/ap-chemistry/redox-reactions-and-electrochemistry-ap",
+        subskillID: 9
+    },
+    {
+        name: "1-D Motion",
+        link: " https://www.khanacademy.org/science/ap-physics-1/ap-one-dimensional-motion",
+        subskillID: 10
+    },
+    {
+        name: "Projectiles",
+        link: " https://www.khanacademy.org/science/ap-physics-1/ap-two-dimensional-motion",
+        subskillID: 10
+    },
+    {
+        name: "Forces",
+        link: " https://www.khanacademy.org/science/ap-physics-1/ap-forces-newtons-laws",
+        subskillID: 11
+    },
+    {
+        name: "Momentum",
+        link: " https://www.khanacademy.org/science/ap-physics-1/ap-linear-momentum",
+        subskillID: 11
+    },
+    {
+        name: "Energy",
+        link: " https://www.khanacademy.org/science/ap-physics-1/ap-work-and-energy",
+        subskillID: 11
+    },
+    {
+        name: "Rotational Motion",
+        link: " https://www.khanacademy.org/science/ap-physics-1/ap-torque-angular-momentum",
+        subskillID: 12
+    },
+    {
+        name: "Simple Harmonic Motion",
+        link: " https://www.khanacademy.org/science/ap-physics-1/simple-harmonic-motion-ap",
+        subskillID: 13
+    },
+    {
+        name: "Circular Motion and Gravity",
+        link: " https://www.khanacademy.org/science/ap-physics-1/ap-centripetal-force-and-gravitation",
+        subskillID: 14
+    },
+    {
+        name: "Basic Charges",
+        link: " https://www.khanacademy.org/science/ap-physics-1/ap-electric-charge-electric-force-and-voltage",
+        subskillID: 15
+    },
+    {
+        name: "Advanced Electricity",
+        link: " https://www.khanacademy.org/science/ap-physics-2/ap-2-electric-charge-electric-force-and-voltage",
+        subskillID: 15
+    },
+    {
+        name: "Magnetic Fields and Forces",
+        link: " https://www.khanacademy.org/science/ap-physics-2/ap-magnetic-forces-and-magnetic-fields",
+        subskillID: 16
+    },
+    {
+        name: "DC Circuits",
+        link: " https://www.khanacademy.org/science/ap-physics-1/ap-circuits-topic",
+        subskillID: 17
+    },
+    {
+        name: "Advanced Circuits",
+        link: " https://www.khanacademy.org/science/ap-physics-2/x0e2f5a2c:ap-2-circuits",
+        subskillID: 17
+    },
+    {
+        name: "Waves and Sound",
+        link: " https://www.khanacademy.org/science/ap-physics-1/ap-mechanical-waves-and-sound",
+        subskillID: 18
+    },
+    {
+        name: "Electromagnetic Waves",
+        link: " https://www.khanacademy.org/science/ap-physics-2/ap-light-waves",
+        subskillID: 19
+    },
+    {
+        name: "Optics",
+        link: " https://www.khanacademy.org/science/ap-physics-2/ap-geometric-optics",
+        subskillID: 20
+    },
+    {
+        name: "Fluids",
+        link: " https://www.khanacademy.org/science/ap-physics-2/ap-fluids",
+        subskillID: 21
+    },
+    {
+        name: "Thermodynamics",
+        link: " https://www.khanacademy.org/science/ap-physics-2/ap-thermodynamics",
+        subskillID: 22
+    },
+    {
+        name: "Quantum Physics",
+        link: " https://www.khanacademy.org/science/ap-physics-2/ap-quantum-physics",
+        subskillID: 23
+    },
+    {
+        name: "Chemistry of Life",
+        link: " https://www.khanacademy.org/science/ap-biology/chemistry-of-life",
+        subskillID: 24
+    },
+    {
+        name: "Cell Structure",
+        link: "https://www.khanacademy.org/science/ap-biology/cell-structure-and-function",
+        subskillID: 25
+    },
+    {
+        name: "Cellular Energetics",
+        link: " https://www.khanacademy.org/science/ap-biology/cellular-energetics",
+        subskillID: 25
+    },
+    {
+        name: "Cell Communication",
+        link: "https://www.khanacademy.org/science/ap-biology/cell-communication-and-cell-cycle",
+        subskillID: 25
+    },
+    {
+        name: "Heredity",
+        link: " https://www.khanacademy.org/science/ap-biology/heredity",
+        subskillID: 26
+    },
+    {
+        name: "Gene Expression and Regulation",
+        link: " https://www.khanacademy.org/science/ap-biology/gene-expression-and-regulation",
+        subskillID: 26
+    },
+    {
+        name: "Natural Selection",
+        link: " https://www.khanacademy.org/science/ap-biology/natural-selection",
+        subskillID: 27
+    },
+    {
+        name: "Ecology",
+        link: " https://www.khanacademy.org/science/ap-biology/ecology-ap",
+        subskillID: 27
+    },
+    {
+        name: "Beginner Series",
+        link: " https://www.youtube.com/playlist?list=PLFE2CE09D83EE3E28",
+        subskillID: 28
+    },
+    {
+        name: "Advanced Series",
+        link: " https://www.youtube.com/playlist?list=PL27BCE863B6A864E3",
+        subskillID: 29
+    },
+    {
+        name: "Python",
+        link: " https://www.youtube.com/watch?v=rfscVS0vtbw",
+        subskillID: 30
+    },
+    {
+        name: "HTML",
+        link: " https://www.youtube.com/watch?v=pQN-pnXPaVg",
+        subskillID: 31 
+    },
+    {
+        name: "CSS",
+        link: " https://www.youtube.com/watch?v=1Rs2ND1ryYc",
+        subskillID: 32
+    },
+    {
+        name: "JavaScript",
+        link: " https://www.youtube.com/watch?v=PkZNo7MFNFg",
+        subskillID: 33
+    },
+    {
+        name: "Solving Equations and Inequalities",
+        link: " https://www.khanacademy.org/math/algebra/x2f8bb11595b61c86:solve-equations-inequalities",
+        subskillID: 34
+    },
+    {
+        name: "System of Equations",
+        link: "https://www.khanacademy.org/math/algebra/x2f8bb11595b61c86:systems-of-equations",
+        subskillID: 35
+    },
+    {
+        name: "Functions",
+        link: " https://www.khanacademy.org/math/algebra/x2f8bb11595b61c86:functions",
+        subskillID: 36
+    },
+    {
+        name: "Sequences",
+        link: " https://www.khanacademy.org/math/algebra/x2f8bb11595b61c86:sequences",
+        subskillID: 36
+    },
+    {
+        name: "Exponents and Radicals",
+        link: " https://www.khanacademy.org/math/algebra/x2f8bb11595b61c86:rational-exponents-radicals",
+        subskillID: 37
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+    {
+        name: "",
+        link: "",
+        subskillID: 
+    },
+]
