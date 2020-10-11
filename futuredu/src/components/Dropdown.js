5856246ff035457e7835477c766a7eea03514bb1
PranavAnand587 <pranav20172017@gmail.com>
2020-10-11 22:51:51 +0530

    made some changes

--- futuredu/src/components/Dropdown.js
+++ futuredu/src/components/Dropdown.js
@@ -5,13 +5,13 @@ const Dropdown = props => {
         <div id="accordion">
             <div className="card">
                 <div className="card-header">
-                    <img className="card-image-top" src={props.image} alt={"Image of " + props.topic} />
+                    {/* <img className="card-image-top" src={props.image} alt={"Image of " + props.topic} /> */}
                     <a className="card-link" data-toggle="collapse" href={"#collapse"+props.id}>
                         {props.topic}
                     </a>
                 <p>Prerequisites : {props.prerequisites}</p>
                 </div>
-                <div id={"collapse"+props.id} className="collapse show" data-parent="#accordion">
+                <div id={"collapse"+props.id} className="collapse" data-parent="#accordion">
                     <div className="card-body">
                         {props.description}
                     </div>
