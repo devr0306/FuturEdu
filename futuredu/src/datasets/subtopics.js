64e0c63df4200c89c311f21c564614aa1adaa397
rcbxd <rbcxdd@gmail.com>
2020-10-10 22:59:13 -0500

    datasets added

---
+++ futuredu/src/datasets/subtopics.js
@@ -0,0 +1,42 @@
+module.exports = [
+    {
+        name: "Chemistry",
+        description: "The study of the world at an atomic level. It is one of the foundations of quantum mechanics, which itself is the foundation of quantum computing.",
+        topicID: 0
+    },
+    {
+        name: "Physics",
+        description: "The field related to the motion of things, whether it be a baseball or a current going through a circuit.",
+        topicID: 0
+    },
+    {
+        name: "Biology",
+        description: "",
+        topicID: 0,
+    },
+    {
+        name: "CS",
+        description: "The field using programming languages and software to mak",
+        topicID: 0,
+    },
+    {
+        name: "Math",
+        description: "",
+        topicID: 0,
+    },
+    {
+        name: "Artificial Intelligence",
+        description: "",
+        topicID: 1
+    },
+    {
+        name: "Deep Learning",
+        description: "",
+        topicID: 1,
+    },
+    {
+        name: "Stock Market",
+        description: "Financial independence is a big problem in the world, where most people don’t use their money wisely. The stock market is a place where financial miracles happen, either good or bad. So, learning about the stock market is important to keep the miracles good, which is what these 3 series do. ",
+        topicID: 2,
+    }
+]
