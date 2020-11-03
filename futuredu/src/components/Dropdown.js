04f14f0a9a7ebc2200246c7b6361d1a29714af9d
PranavAnand587 <pranav20172017@gmail.com>
2020-11-04 01:47:12 +0530

    Finished the Level 3 Dropdowns

--- futuredu/src/components/Dropdown.js
+++ futuredu/src/components/Dropdown.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Markdown from 'markdown-to-jsx';
 
 const Dropdown = props => {
+
+    const [isToggle, setToggle] = useState(false)
+
+    const toggleTrueFalse = () => setToggle(!isToggle)
+
     return (
-        <div className="card-body pl-5 py-4">
-            <p className="card-text my-2">{<Markdown>{props.description}</Markdown>}</p>
+        <div className="row">
+            <div className="col-10">
+                <div className="card-body pl-5 py-3" id={props.id}>
+                    <h5 className="card-title py-2">{props.name}</h5>
+                    <p className="card-text my-2" id={props.id + "collapse"}>{<Markdown>{props.description}</Markdown>}</p>
+                </div>
+            </div>
+            <div className="col-2 text-center">
+                <div className="mt-4" onClick={toggleTrueFalse}>
+                        <button
+                            className="fas fa-angle-down visible"
+                            data-fa-transform={isToggle ? "rotate-90" : null}
+                            type="button"
+                            data-toggle="collapse"
+                            data-target={"#" + props.id + "collapse"}
+                        />
+                </div>
+            </div>
         </div>
+
     )
 }
+
 
 export default Dropdown;
